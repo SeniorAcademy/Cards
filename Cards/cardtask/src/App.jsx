b46a94766813c4774b68d2a129ce1e66d934cd6a
SeniorAcademy <seniorazacademy@gmail.com>
2024-02-29 19:33:44 +0400

    Lesson_2

---
+++ Cards/cardtask/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./components/Home";
+import Count from "./components/Count";
+
+function App() {
+  return (
+    <>
+      {/* <Home /> */}
+      <Count />
+
+    </>
+  );
+}
+
+export default App;
